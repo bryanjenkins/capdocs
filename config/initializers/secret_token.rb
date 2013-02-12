2a3b76786be3c43cfd66705deaa1d499627f5430
Bryan Jenkins <bryancjenkins@gmail.com>
2013-02-12 12:18:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capdocs::Application.config.secret_token = 'c2286e521a0f1e32aa13179dbdb7f87c6750f1009c6691290ede86154e12f549602fb847686ff928bbdc8388db2f909b4184c9ff58b5369e6c2f15c85e6f39c8'
